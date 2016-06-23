d5a6878ab2f09672c446d9a78126f034a8606c84
Matthew Wilton <matthewrwilton@gmail.com>
2016-06-23 22:10:07 +1000

    Simplify UI flow.

--- src/index.ts
+++ src/index.ts
@@ -3,49 +3,75 @@ import PdfScraper from "./PdfScraping/PdfScraper";
 import CsvConverter from "./Statements/Converting/CsvConverter";
 import StatementParser from "./Statements/Parsing/StatementParser";
 
-const convertButtonSelector = "#convert-to-csv-button";
+const retryButtonSelector = "#retry-button";
 const filePickerSelector = "#statement-file-picker";
-const passwordInputLabel = "#password-input-label";
-const passwordSection = "#password-section";
+const passwordSectionSelector = "#password-section";
+const passwordInputLabelSelector = "#password-input-label";
+const passwordInputSelector = "#password-input";
 
-$(convertButtonSelector).click(() => {
-	let filePicker = $(filePickerSelector).get(0),
-        pdfFiles = (<HTMLInputElement>filePicker).files,
-        pdfScraper = new PdfScraper(),
-        password = (<HTMLInputElement>$("#statement-password").get(0)).value;
+$(filePickerSelector).on("change", convertPdf);
+$(retryButtonSelector).click(convertPdf);
 
-    for (let i = 0; i < pdfFiles.length; i++) {
-        let pdfFile = pdfFiles[i];
-        let pdfTextPromise = pdfScraper.scrapeText(window.URL.createObjectURL(pdfFile), password)
-            .then(result => {
-                if (result.successful) {
-                    let statementParser = new StatementParser(),
-                        statementItems = statementParser.parse(result.text),
-                        csvConverter = new CsvConverter(),
-                        csvContent = csvConverter.convert(statementItems).join("\r\n"),
-                        encodedCsvContent = encodeURIComponent(csvContent),
-                        csvUri = "data:text/csv;charset=utf-8," + encodedCsvContent,
-                        downloadLink = document.createElement("a"),
-                        filename = `${pdfFile.name.substring(0, pdfFile.name.lastIndexOf(".pdf"))}.csv`;
-                    
-                    // A link is created instead of redirecting to the URI so that a filename can be set.
-                    // Setting the href to the URI and download to the filename comes from FileSaver.js (via StackOverflow).
-                    downloadLink.href = csvUri;
-                    downloadLink.style.cssText = "visibility: hidden";
-                    downloadLink.download = filename;
+function convertPdf() {
+    let filePicker = <HTMLInputElement>$(filePickerSelector).get(0),
+        password = (<HTMLInputElement>$(passwordInputSelector).get(0)).value,
+        pdfScraper = new PdfScraper();
 
-                    document.body.appendChild(downloadLink);
-                    downloadLink.click();
-                    document.body.removeChild(downloadLink);
-                }
-                else if (result.passwordRequired) {
-                    $(passwordSection).show();
-                    $(passwordInputLabel).text("Statement password required");
-                }
-                else if (result.passwordIncorrect) {
-                    $(passwordSection).show();
-                    $(passwordInputLabel).text("Incorrect password");
-                }
-            })
+    if (filePicker.files.length == 0) {
+        clearAndHidePasswordInput();
+        return;
     }
-});
+
+    let pdfFile = filePicker.files[0];
+    pdfScraper.scrapeText(window.URL.createObjectURL(pdfFile), password).then(result => {
+        if (result.successful) {
+            let statementParser = new StatementParser(),
+                statementItems = statementParser.parse(result.text),
+                csvConverter = new CsvConverter(),
+                csvContent = csvConverter.convert(statementItems).join("\r\n"),
+                encodedCsvContent = encodeURIComponent(csvContent);
+
+            downloadCsv(pdfFile.name, encodedCsvContent);
+
+            clearAndHidePasswordInput();
+        }
+        else if (result.passwordRequired) {
+            showPasswordRequiredError();
+        }
+        else if (result.passwordIncorrect) {
+            showIncorrectPasswordError();
+        }
+    });
+}
+
+function downloadCsv(pdfFileName: string, encodedCsvContent: string) {
+    let downloadLink = document.createElement("a"),
+        csvUri = "data:text/csv;charset=utf-8," + encodedCsvContent,
+        filename = `${pdfFileName.substring(0, pdfFileName.lastIndexOf(".pdf"))}.csv`;
+
+    // A link is created instead of redirecting to the URI so that a filename can be set.
+    // Setting the href to the URI and download to the filename comes from FileSaver.js (via StackOverflow).
+    downloadLink.href = csvUri;
+    downloadLink.style.cssText = "visibility: hidden";
+    downloadLink.download = filename;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
+function clearAndHidePasswordInput() {
+    $(passwordSectionSelector).hide();
+    (<HTMLInputElement>$(passwordInputSelector).get(0)).value = "";
+}
+
+function showPasswordRequiredError() {
+    $(passwordInputLabelSelector).text("Statement password required");
+    $(passwordSectionSelector).show();
+}
+
+function showIncorrectPasswordError() {
+    $(passwordInputLabelSelector).text("Incorrect password");
+    (<HTMLInputElement>$(passwordInputSelector).get(0)).value = "";
+    $(passwordSectionSelector).show();
+}
